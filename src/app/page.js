4cb50bd731996963982e6cc39e9f008232fd13c7
Sanjana Kumari <sanjana@Sanjanas-MacBook-Air.local>
2025-08-28 12:25:34 +0530

    initial commit

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,11 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+    <div className="flex items-center justify-center min-h-screen">
+      <p className="text-green-600 text-4xl text-center">
+        Helloooo!!, welcome to my Portfolio.
+      </p>
+      {/* <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
           src="/next.svg"
@@ -97,7 +100,7 @@ export default function Home() {
           />
           Go to nextjs.org →
         </a>
-      </footer>
+      </footer> */}
     </div>
   );
 }
