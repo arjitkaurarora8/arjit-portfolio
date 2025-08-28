ec0ec78d14f0db1cb483c3b45db258fd1007688b
Sanjana Kumari <sanjana@Sanjanas-MacBook-Air.local>
2025-08-29 02:15:27 +0530

    create navbar

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Navbar from "@/component/Navbar";
+
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
       <Navbar />
+
       <div className="flex items-center justify-center min-h-screen">
         <p className="text-green-600 text-4xl text-center">
           Helloooo!!, welcome to my Portfolio.
